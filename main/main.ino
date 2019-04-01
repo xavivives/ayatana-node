db1c5946d98fd25fa3cb4a4a14542d52fcbd94c8
Xavi <xavings@gmail.com>
2019-04-01 19:17:32 +0000

    Correcting sensor

--- main/main.ino
+++ main/main.ino
@@ -10,9 +10,9 @@ const float vcc = 3.15;
 const float z1 = 178000;
 const float z2 = 270000;
 const float readOffset = -7;
-const float sensorOffset = 0.45;
+const float sensorOffset = -0.72;
 
-HomieNode currentNode("relay", "switch");
+HomieNode currentNode("CurrentSensor", "current");
 
 void setup()
 {
@@ -33,61 +33,25 @@ void loop()
   currentMeter.cycle();
 }
 
-float tensionDivider(float v, float z1, float z2)
-{
-  return z2 / (z1 + z2) * v;
-}
-
-float correctReadOffset(float v)
-{
-  return v + readOffset;
-}
-
-float readToVolts(float v)
-{
-  return v * 3.3 / 1023;
-}
-
-float correctSensorOffset(float v)
-{
-  return v + sensorOffset;
-}
-
-float tensionMultiplier(float v, float z1, float z2)
-{
-  return v / (z2 / (z1 + z2));
-}
-
-float tensionToCurrent (float v)
-{
-  return (v-2.5)/(66/1000);
-}
-
 void onCurrentChange(int idx, int v, int up)
 {
-
-  //float v1 = correctReadOffset(v);
-  //float v2 = readToVolts(v1);
-  //float v3 = tensionMultiplier(v2, z1, z2);
-  //float v4 = tensionToCurrent(v3);
-
   //float vcc = ((float)ESP.getVcc())/1024;
   float zeroCurrentVoltage = 5/2; //Get real tension
 
   float cleanRead = v + readOffset;
-  float sensedVoltage = cleanRead * vcc / 1024; //digital read to volts
-  float tensionMultiplier = sensedVoltage / (z2 / (z1 + z2));
-  float difference = tensionMultiplier - zeroCurrentVoltage;
+  float readVoltage = cleanRead * vcc / 1024; //digital read to volts
+  float tensionMultiplier = readVoltage / (z2 / (z1 + z2));
+  float cleanSensed = tensionMultiplier + sensorOffset;
+  float difference = cleanSensed - zeroCurrentVoltage;
   float tensionToCurrent = difference/(66/1000);
   
-
   Serial << "\nRead: " << v
     << ", NoOffset: " << cleanRead
-    << ", sensedVoltage: "<< sensedVoltage
+    << ", readVoltage: "<< readVoltage
     << ", TensionMultiplier: " << tensionMultiplier
+    << ", CleanSensed: "<< cleanSensed
+    << ", Difference: " << difference
     <<", Current: "<< tensionToCurrent
     <<", Vcc: "<< vcc
     << endl;
-
-  // Do something when the analog value changes
 }
