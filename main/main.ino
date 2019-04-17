488a31e35b983a0b064b8b51bc83fc7c626967bf
Xavi <xavings@gmail.com>
2019-04-17 18:08:06 +0000

    Testing

--- main/main.ino
+++ main/main.ino
@@ -22,17 +22,16 @@ void setup()
          << endl;
 
   Homie_setFirmware("airSensor", "1.0.0");
-  Homie.setSetupFunction(setupHandler);
-  Homie.setLoopFunction(loopHandler);
+  airNode.advertise("temperature");
+  airNode.advertise("trigger").settable(onTrigger);
+  Homie.setup();
 
   /*
   Wire.begin(D6,D5); // Define which ESP8266 pins to use for SDA, SCL of the Sensor
   Wire.setClock(100000);    // Set I2C bus speed 
-  
-  if (!bme.begin(0x76)) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-  }
   */
+ 
+ 
 
   //delay(100);
 
@@ -41,10 +40,13 @@ void setup()
       .onTimer(timer_callback)
       .start();
 */
-  airNode.advertise("air");
-  airNode.advertise("trigger").settable(onTrigger);
-  Homie.setup();
+ 
   Homie.getLogger() << "Homie logger setup: " << endl;
+
+  //if (!bme.begin(0x76)) {
+  //  Serial.println("Could not find a valid BME280 sensor, check wiring!");
+  //}
+  
 }
 
 bool onTrigger(const HomieRange &range, const String &value)
@@ -59,17 +61,6 @@ bool onTrigger(const HomieRange &range, const String &value)
   Homie.getLogger() << "Relay is " << (on ? "on" : "off") << endl;
 
   return true;
-}
-
-void setupHandler()
-{
-  Serial << "\n Connected to MQTT "<< endl;
-  
-}
-
-void loopHandler()
-{
-  //Serial << "\n Looped after MQTT connection"<< endl;
 }
 
 void loop()
@@ -104,7 +95,7 @@ void read()
          << endl;
          */
   Serial << "\n Read"<< endl;
-  Homie.getLogger() << "Temperature: " << temperature << " °C" << endl;
+  Homie.getLogger() << "Temp: " << temperature << " °C" << endl;
   airNode.setProperty("temperature").send(String(temperature));
   //airNode.setProperty("humidity").send(String(humidity));
   //airNode.setProperty("pressure").send(String(pressure));
